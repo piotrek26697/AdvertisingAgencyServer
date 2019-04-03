36842f5531ad159d3354012eddbdc8dd97be6061
Piotr Musioł (piotmus099) <piotmus099@student.polsl.pl>
2019-04-03 21:57:02 +0200

    Basic *.xml config

---
+++ src/entities/Client.java
@@ -0,0 +1,59 @@
+package entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.Serializable;
+
+@XmlRootElement
+@Entity
+public class Client implements Serializable
+{
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private String lastName;
+    private String address;
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public String getAddress()
+    {
+        return address;
+    }
+
+    public void setAddress(String address)
+    {
+        this.address = address;
+    }
+}
