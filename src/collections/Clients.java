6e090aaa93372077fe14603644938f53a1c2b11d
Piotr Musioł (piotmus099) <piotmus099@student.polsl.pl>
2019-04-11 17:32:40 +0200

    Constructor added for class Clients.

--- src/collections/Clients.java
+++ src/collections/Clients.java
@@ -11,6 +11,11 @@ public class Clients implements Serializable
 {
     List<Client> clientList;
 
+    public Clients(List<Client> clientList)
+    {
+        this.clientList = clientList;
+    }
+
     public List<Client> getClientList()
     {
         return clientList;
