8915726353ba026e2343d96f07413690e0135cb1
piotrek26697 <piotrek26697@gmail.com>
2019-04-28 22:03:53 +0200

    Update InvoiceHandlingREST.java

--- src/REST/InvoiceHandlingREST.java
+++ src/REST/InvoiceHandlingREST.java
@@ -18,28 +18,5 @@ public class InvoiceHandlingREST
     @EJB
     private InvoiceHandlingEJB invoiceBean;
 
-    @POST
-    public void createInvoice(InputStream in)
-    {
-        Invoice invoice = JAXB.unmarshal(in, Invoice.class);
-        invoiceBean.create(invoice);
-    }
-
-    @PUT
-    public void updateInvoice(InputStream in)
-    {
-        Invoice invoice = JAXB.unmarshal(in, Invoice.class);
-        invoiceBean.update(invoice);
-    }
-
-    @GET
-    public String getAdvertisementList(@QueryParam("invoiceID") int invoiceID)
-    {
-        List<Advertisement> list = invoiceBean.getAdvertisementList(invoiceID);
-        Advertisements ads = new Advertisements();
-        StringWriter sw = new StringWriter();
-        ads.setAdsList(list);
-        JAXB.marshal(ads, sw);
-        return sw.toString();
-    }
+    
 }
